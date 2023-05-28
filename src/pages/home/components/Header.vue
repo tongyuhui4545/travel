1d53f74ead2a47f97c6a92e940a3b07b1bd90c39
tongyuhui4545 <tongyuhui4545@gmail.com>
2023-05-28 14:31:56 +0800

    优化了目录

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -20,10 +20,11 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
+@import '~styles/variables.styl'
   .header
     display: flex
     line-height: .86rem
-    background: #00bcd4
+    background: $bgColor
     color: #fff
     .header-left
       width: .64rem
